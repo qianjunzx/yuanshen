a371d820e01bbe7436efcb772185545f9c927267
qianjunzx <84954625+qianjunzx@users.noreply.github.com>
2021-05-29 01:34:35 +0800

    套件识别与映射

--- relic/analyze.py
+++ relic/analyze.py
@@ -24,16 +24,18 @@ def relic_board(relic_condition):
 
 entry_type={
     '攻击力':        1,
-    '攻击力百分比':  2,
+    '攻击力百分比':   2,
     '暴击率':        3,
     '暴击伤害':      4,
     '防御力':        7,
     '防御力百分比':  8,
-    '生命值':        9,
+    '生命值':       9,
     '生命值百分比': 10,
     '治疗加成':     11,
     '元素精通':     12,
- }
+    '元素充能效率': 13,
+    '护盾强效':     16,
+}
 
 def isdamage(a):
     if a == '火元素伤害加成':
@@ -52,8 +54,37 @@ def isdamage(a):
         return 7
     elif a == '物理伤害加成':
         return 8
+    elif a== '元素战技伤害加成':
+        return 9
+    elif a== '元素爆发伤害加成':
+        return 10
+    elif a== '普通攻击伤害加成':
+        return 11
+    elif a== '重击伤害加成':
+        return 12
+    elif a== '伤害加成':
+        return 13
     else:
         return 0
+
+suit_effort={           #确定套件的加成【两件套大类（1.普通加成2.伤害加成3.特殊处理），两天套小类，两件套数值，四件套大类，四件套小类，四件套数值】
+    '冰风迷途的勇士'  : [2,3,15,1,3,40],
+    '平息鸣雷的尊者'  : [0,0,0,2,4,35],
+    '渡过烈火的贤人'  : [0,0,0,2,1,35],
+    '被怜爱的少女'    : [1,11,15,1,11,20],
+    '角斗士的终幕礼'   :[1,2,18,2,11,35],
+    '翠绿之影'        : [2,6,15,3,0,0],#unfinished
+    '流浪大地的乐团'   :[1,13,80,2,12,35],
+    '如雷的盛怒'      : [2,4,15,3,0,0],#unfinished
+    '炽烈的炎之魔女'    : [2,1,15,2,1,7.5],#unfinishied
+    '昔日之宗室'        :[2,10,20,1,2,20],
+    '染血的骑士'        :[2,8,25,3,0,0],#unfinished
+    '悠古的磐岩'        :[2,7,15,3,0,0],#unfinished
+    '逆飞的流行'        :[1,16,35,2,13,35],
+    '千岩牢固'          :[1,10,20,1,2,30],#unfinished
+    '苍白之火'          :[2,8,25,2,8,25,12,18],
+}
+
 def relic_suit(relic_condition):#查看套件类型
     suit={}
     suit_type=[]
@@ -64,7 +95,5 @@ def relic_suit(relic_condition):#查看套件类型
         if suit[cell]==1:
             suit_type.append(cell)
     return suit,suit_type
-def suit_board_plus(suit,suit_type): #套件效果计算 
-    pass
-def abab():
+def suit_condition(a,b,c):
     pass
