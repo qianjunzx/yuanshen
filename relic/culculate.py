18ba50907d5851eb83cc727616825a25d9fa80e5
qianjunzx <84954625+qianjunzx@users.noreply.github.com>
2021-06-03 17:16:55 +0800

    Update culculate.py

---
+++ relic/culculate.py
@@ -0,0 +1,8 @@
+class fundmantal_board(object):
+
+    def input(self,entry_type,value):#输入面板数据的方法
+        self.dic[entry_type] += value
+
+    def output(self,entry_type):#输出面板数据方法
+        return self.dic[entry_type]
+
